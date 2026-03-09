8f8663d6a696f5a10c6170618b7fb42922e49a72
ilefkristou992@gmail.com <ilefkristou992@gmail.com>
2026-03-09 11:02:27 +0100

    ajout metrics

---
+++ app/api/metrics/route.ts
@@ -0,0 +1,14 @@
+import { NextResponse } from 'next/server';
+import client from 'prom-client';
+
+const register = new client.Registry();
+client.collectDefaultMetrics({ register });
+
+export async function GET() {
+    const metrics = await register.metrics();
+    return new NextResponse(metrics, {
+        headers: {
+            'Content-Type': register.contentType,
+        },
+    });
+}
